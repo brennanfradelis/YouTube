6d1fd5c075b25927b196e5cdb0d1895a17fd51db
brennanfradelis <brennanfradelis@gmail.com>
2021-01-03 20:06:39 -0800

    Add hasCycle function

--- Coding-Interview-Patterns/JavaScript/5-Cycle-Detection.js
+++ Coding-Interview-Patterns/JavaScript/5-Cycle-Detection.js
@@ -1,11 +1,21 @@
 //Video Link: https://youtu.be/PR-t0sUTddw
+
 class TreeNode {
     constructor(value) {
         this.value = value;
         this.next = null;
     }
 }
-function hasCycleBruteForce(node) {
-    s = set()
+
+function hasCycle(node) {
+    var slow = head;
+    var fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == fast) {
+            return true;
+        }
+    }
     return false;
 }
