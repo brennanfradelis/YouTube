64d759001a6e1ffbeeafd4f22baacb99f0d4f959
brennanfradelis <brennanfradelis@gmail.com>
2021-01-03 16:51:43 -0800

    Add 5-Cycle-Detection.js

---
+++ Coding-Interview-Patterns/JavaScript/5-Cycle-Detection.js
@@ -0,0 +1,10 @@
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+function hasCycleBruteForce(node) {
+    s = set()
+    return false;
+}
